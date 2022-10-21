7bf086787890a733443d2bd7bad0994faa86e6a7
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-10-22 00:14:18 +0530

    Added Notebook

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import $ from 'jquery';
 import {Converter} from 'showdown';
 import ClickEvent = JQuery.ClickEvent;
+import type {Notebook} from "./notebook/notes";
 
 const converter = new Converter();
+
+export const notebook: Notebook = {
+    title: "demo",
+    content: [],
+    size: 0,
+    currSec: -1,
+    currChapt: -1
+};
 
 $('.btn-preview').on('click', (event: ClickEvent) => {
     parent = event.target.parentElement.id;
@@ -11,8 +20,25 @@ $('.btn-preview').on('click', (event: ClickEvent) => {
     let previewSelection: string = `#${parent} > .markdown-preview`;
     let preview: string = converter.makeHtml(content);
     $(previewSelection).html(preview);
-
 });
 
+const addSection = (title: string) => {
+    notebook.content.push({
+        index: notebook.size,
+        title: title,
+        size: 0,
+        chapters: []
+    });
+    notebook.size++;
+}
+const addChapter = (title: string) => {
+    notebook.content[notebook.currSec].chapters.push({
+        index: notebook.content[notebook.currSec].size,
+        title: title,
+        notes: [],
+        size: 0
+    });
+    notebook.content[notebook.currSec].size++;
+}
 
 
---
+++ src/notebook/notes.ts
@@ -0,0 +1,26 @@
+export type Note = {
+    index: number,
+    title: string,
+    content: string
+}
+
+export type Chapter = {
+    index: number,
+    title: string;
+    notes: Array<Note>,
+    size: number
+};
+export type Section = {
+    index: number,
+    title: string,
+    chapters: Array<Chapter>,
+    size: number
+};
+
+export type Notebook = {
+    title: string,
+    content: Array<Section>,
+    size: number,
+    currSec: number,
+    currChapt: number
+}
